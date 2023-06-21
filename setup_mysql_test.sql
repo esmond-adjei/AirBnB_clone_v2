6bb0cd5ebc6317146da7ea3604cde6fa92b4eb29
esmond-adjei <xmongram@gmail.com>
2023-06-21 12:47:52 +0000

    modified some files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,8 @@
 -- test user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT USAGE ON *.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 
 
