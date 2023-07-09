3a401e78bb7c37e8b2bd17a0d0ae367dd3a56343
esmond-adjei <xmongram@gmail.com>
2023-07-09 14:57:14 +0000

    modified some files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,14 @@ fi
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "<h1>Holberton School</h1>" > /data/web_static/releases/test/index.html
+echo "
+<html>
+<head></head>
+<body>
+  Holberton School
+</body>
+</html>
+" > /data/web_static/releases/test/index.html
 
 rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
