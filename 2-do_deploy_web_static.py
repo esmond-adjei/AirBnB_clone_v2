a9d88d132aefe36bc19c581aefad7f54c4c03ad3
esmond-adjei <xmongram@gmail.com>
2023-07-09 17:31:26 +0000

    modified some files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-# Deploys website source code to servers
 from fabric.api import env, put, run
 from os.path import exists
 
-env.hosts = ['18.207.233.152', '100.26.221.176']
+env.hosts = ['<IP web-01>', '<IP web-02>']
+
 
 def do_deploy(archive_path):
     """
@@ -15,16 +14,20 @@ def do_deploy(archive_path):
 
     try:
         filename = archive_path.split('/')[-1]
-        filename_no_ext = filename.split('.')[0]
+        fname = filename.split('.')[0]
 
-        put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}/'.format(filename_no_ext))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, filename_no_ext))
+        put(archive_path, '/tmp/{}'.format(filename))
+        run('mkdir -p /data/web_static/releases/{}/'.format(fname))
+        run('tar -xzf /tmp/{} -C'
+            '/data/web_static/releases/{}/'.format(filename, fname))
         run('rm /tmp/{}'.format(filename))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(filename_no_ext, filename_no_ext))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(filename_no_ext))
+        run('mv /data/web_static/releases/{}/web_static/*'
+            '/data/web_static/releases/{}/'.format(fname, fname))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(fname))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(filename_no_ext))
+        run('ln -s /data/web_static/releases/{}/'
+            '/data/web_static/current'.format(fname))
+
         print('New version deployed!')
         return True
     except Exception as e:
